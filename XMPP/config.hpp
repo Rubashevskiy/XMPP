1f85550b7a1cc7b672e64ce8a27252952a5deec1
Rubashevskiy Yuriy <r9182230628@gmail.com>
2021-10-07 00:00:34 +0300

    update read config

--- XMPP/config.hpp
+++ XMPP/config.hpp
@@ -31,35 +31,63 @@ namespace XMPPBUS {
       if (resource.empty()) return jid + "@" + host;
       else return jid + "@" + host + "/" + resource;
     }
-    
+        static std::string nodeToStr(const pugi::xml_node &node) {
+      if (node) {
+        return std::string(node.child_value());
+      }
+      else return std::string("");
+    };
+
+    static int nodeToInt(const pugi::xml_node &node) {
+      if (node) {
+        try {
+          return std::stoi(std::string(node.child_value()));
+        }
+        catch (...) {
+          return 0;
+        }
+      }
+      else return 0;
+    };
+
+    static bool nodeToBool(const pugi::xml_node &node) {
+      if (node) {
+        std::string check = std::string(node.child_value());
+        if (("true" == check) || ("True" == check) || ("1" == check)) return true;
+        else return false;
+      }
+      else return false;
+    };
+
+    static AuthType nodeToAuth(const pugi::xml_node &node) {
+      if (node) {
+        std::string check = std::string(node.child_value());
+        if ("DigestMD5" == check) return AuthType::DigestMD5;
+        else if ("SCRAM_SHA1" == check) return AuthType::SCRAM_SHA1;
+        else if ("Plain" == check) return AuthType::Plain;
+        else return AuthType::AUTO;
+      }
+      else return AuthType::AUTO;
+    };
+
     bool read(const std::string &config_path) {
       try {
         pugi::xml_document xdoc;
         pugi::xml_parse_result load_result = xdoc.load_file(config_path.c_str(), pugi::parse_full&~pugi::parse_eol, pugi::encoding_auto);
         if (!load_result) return false;
         std::string buffer;
-        pugi::xml_node jid_node = xdoc.select_single_node("//jid").node();
-        jid = xdoc.select_single_node("//jid").node().child_value();
-        host = xdoc.select_single_node("//host").node().child_value();
-        buffer = xdoc.select_single_node("//port").node().child_value();
-        port = std::stoi(buffer);
-        password = xdoc.select_single_node("//password").node().child_value();
-        buffer = xdoc.select_single_node("//auth").node().child_value();
-        if ("DigestMD5" == buffer) auth = AuthType::DigestMD5;
-        else if ("SCRAM_SHA1" == buffer) auth = AuthType::SCRAM_SHA1;
-        else if ("Plain" == buffer) auth = AuthType::Plain;
-        else auth = AuthType::AUTO;
-        buffer = xdoc.select_single_node("//ssl").node().child_value();
-        ssl = ("true" == buffer) ? (true) : (false);
-        resource = xdoc.select_single_node("//resource").node().child_value();
-        show_status = xdoc.select_single_node("//show_status").node().child_value();
-        msg_status = xdoc.select_single_node("//msg_status").node().child_value();
-        buffer = xdoc.select_single_node("//priority").node().child_value();
-        priority = std::stoi(buffer);
-        buffer = xdoc.select_single_node("//keep_alive").node().child_value();
-        keep_alive = ("true" == buffer) ? (true) : (false);
-        buffer = xdoc.select_single_node("//debug").node().child_value();
-        debug = ("true" == buffer) ? (true) : (false);
+        jid = nodeToStr(xdoc.select_single_node("//jid").node());
+        host = nodeToStr(xdoc.select_single_node("//host").node());
+        port = nodeToInt(xdoc.select_single_node("//port").node());
+        password = nodeToStr(xdoc.select_single_node("//password").node());
+        auth = nodeToAuth(xdoc.select_single_node("//auth").node());
+        ssl = nodeToBool(xdoc.select_single_node("//ssl").node());
+        resource = nodeToStr(xdoc.select_single_node("//resource").node());
+        show_status = nodeToStr(xdoc.select_single_node("//show_status").node());
+        msg_status = nodeToStr(xdoc.select_single_node("//msg_status").node());
+        priority = nodeToInt(xdoc.select_single_node("//priority").node());
+        keep_alive = nodeToBool(xdoc.select_single_node("//keep_alive").node());
+        debug = nodeToBool(xdoc.select_single_node("//debug").node());
         return true;
       }
       catch (...) {
@@ -67,7 +95,6 @@ namespace XMPPBUS {
       }
 
     }
-
     std::string jid;
     std::string host;
     std::string password;
